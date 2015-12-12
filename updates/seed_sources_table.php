f0d351082dfb41781bd572a4ab16b5dbc0affaad
Alwin Drenth <alwin@vdlp.nl>
2015-12-12 14:00:13 +0100

    Add another source example in source seeder

--- updates/seed_sources_table.php
+++ updates/seed_sources_table.php
@@ -21,14 +21,21 @@ class SeedAllTables extends Seeder
             'name' => 'NU.nl | Algemeen nieuws',
             'description' => 'NU.nl | Algemeen nieuws',
             'source_url' => 'http://www.nu.nl/rss/Algemeen',
-            'max_items' => 0,
+            'max_items' => 10,
             'is_enabled' => true,
         ]);
         Source::create([
             'name' => 'NU.nl | Internet',
             'description' => 'NU.nl | Internet',
             'source_url' => 'http://www.nu.nl/rss/Internet',
-            'max_items' => 0,
+            'max_items' => 10,
+            'is_enabled' => true,
+        ]);
+        Source::create([
+            'name' => 'Tweakers.net',
+            'description' => 'Tweakers.net is sinds 1998 de grootste website in Nederland over technologie en elektronica met nieuws, reviews en de bekroonde Pricewatch.',
+            'source_url' => 'http://feeds.feedburner.com/tweakers/mixed',
+            'max_items' => 10,
             'is_enabled' => true,
         ]);
     }
